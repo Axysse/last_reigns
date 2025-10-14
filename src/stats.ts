29e389d69ed71f23c739c506328eaf670059330b
Nicolas VOUILLEROT <NVOUILLEROT@NAHEULBEUK.ACAD>
2025-10-14 09:00:53 +0200

    dernier espoir

--- src/stats.ts
+++ src/stats.ts
@@ -1,5 +1,5 @@
 import { getAdjacentCells } from "./main";
-import { defeat } from "./gameOver";
+import { defeat } from "./gameover";
 
 const nourritureDisplay: HTMLParagraphElement | null = document.getElementById(
   "nourritureDisplay"
